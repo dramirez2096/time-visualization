f1e8b3d57ab2ed4e6880f127436b177efd2e257e
David Ramirez <dramirez2096@gmail.com>
2018-02-10 19:05:32 -0800

    linked to p5 library, sketch.js file created

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(window.innerWidth, window.innerHeight);
+  }
+  
+  function draw() {
+  }
+  
+  function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+  }
