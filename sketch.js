7a15b7e5269b25f4e4b66b72afcc1ed699c4ecd5
David Ramirez <dramirez2096@gmail.com>
2018-02-12 16:31:35 -0800

    static yellow circle created

--- sketch.js
+++ sketch.js
@@ -4,7 +4,13 @@ function setup() {
   
   function draw() {
       strokeWeight(3);
+      noFill();
       ellipse(windowWidth/2, windowHeight/2, 550, 550);
+
+      //yellow - seconds circle
+      noStroke();
+      fill(255,255,0);
+      ellipse(windowWidth/2, windowHeight/2, 100, 100);
   }
   
   function windowResized() {
