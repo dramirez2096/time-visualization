3f5b64579cc42b4feaa2533f0ef773c8c1aebff5
David Ramirez <dramirez2096@gmail.com>
2018-02-12 17:32:07 -0800

    yellow circle rotates

--- sketch.js
+++ sketch.js
@@ -1,16 +1,27 @@
+var angle = 0;
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
+    angleMode(DEGREES);
   }
   
   function draw() {
+      background(255);
+
       strokeWeight(3);
-      noFill();
+      fill(255);
       ellipse(windowWidth/2, windowHeight/2, 550, 550);
 
       //yellow - seconds circle
-      noStroke();
+      translate(windowWidth/2, windowHeight/2);
+      rotate(angle);
+
       fill(255,255,0);
-      ellipse(windowWidth/2, windowHeight/2, 100, 100);
+      //ellipseMode(CENTER);
+      ellipse(windowHeight/27 + 12, windowWidth/27 - 12, 100, 100);
+      line(0,0,200,200);
+
+      
+      angle = angle + 5;
   }
   
   function windowResized() {
