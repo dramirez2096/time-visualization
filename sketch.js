36672026e4b51967ed9eebc00442990deb2aed7e
David Ramirez <dramirez2096@gmail.com>
2018-02-14 16:14:59 -0800

    red circle created, stroke removed from inner circles

--- sketch.js
+++ sketch.js
@@ -1,4 +1,6 @@
 var angle = 0;
+var sec = 0;
+
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
     angleMode(DEGREES);
@@ -12,20 +14,25 @@ function setup() {
       fill(255);
       ellipse(windowWidth/2, windowHeight/2, 550, 550);
 
-      //yellow - seconds circle
       translate(windowWidth/2, windowHeight/2);
-      //rotate(angle);
+      rotate(angle);
 
-      fill(255,255,0);
+      strokeWeight(0);
+      //yellow - seconds circle
+      fill(255,255,0,100);
       ellipse(0,0 - 50, 100, 100, 50);
 
       //blue - minutes circle
-      fill(0,201,255,50);
+      fill(0,201,255,100,);
       ellipse(0,0 - 100, 200, 200);
+      
+      //red - hours circle
+      fill(255,0,0,100)
+      ellipse(0,0 - 137, 275, 275)
+
       line(0,0,0,0);
-
-
-      angle = angle + 5;
+      
+      angle = angle;
   }
   
   function windowResized() {
